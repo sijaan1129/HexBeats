868223e8c9b1fc199bbfa2401ea78f13d73431bf
sijaan1129 <sijaan53@gmail.com>
2025-04-19 14:42:45 +1200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://aimbot:Gs9caKIFRd2VN4yH@discordbot.nofuzan.mongodb.net/?retryWrites=true&w=majority&appName=DiscordBot",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
